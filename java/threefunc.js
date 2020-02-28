2888014f129e1b5153f15b5dde4ddb0c69b981f2
reyanlee <anni3lee03@gmail.com>
2020-02-28 09:14:10 -1000

    first commit

---
+++ java/threefunc.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  window.open("index2.html");
+}
+
+function clickImg() {
+	window.open("index2.html");
+}
+
+function creditButton() {
+	window.open("index3.html")
+}
+
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
